1b8f6682d357753f823ef83805ac0c904fb9e31d
Alex Alan Nunes <alexalannunes@gmail.com>
2022-11-27 11:28:51 -0300

    refactor: types

---
+++ types/index.ts
@@ -0,0 +1,1 @@
+export type { Shadow } from "./shadow";
---
+++ types/shadow.ts
@@ -0,0 +1,10 @@
+export type Shadow = {
+  id: string;
+  x: number;
+  y: number;
+  blur: number;
+  spread: number;
+  inset: boolean;
+  active: boolean;
+  color: string;
+};
