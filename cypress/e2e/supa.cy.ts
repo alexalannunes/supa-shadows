35e4ed7bed87e093810a8a41f147a3052c0a37c8
Alex Alan Nunes <alexalannunes@gmail.com>
2022-11-28 17:33:15 -0300

    test: toggle shadow item

--- cypress/e2e/supa.cy.ts
+++ cypress/e2e/supa.cy.ts
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe("supa shadows", () => {
+  it("toggle shadow item", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-testid="accordion-item-0"]').click();
+    cy.get('[data-testid="accordion-item-0"]').should(
+      "have.attr",
+      "aria-expanded",
+      "false"
+    );
+  });
+});
