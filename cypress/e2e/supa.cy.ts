0aedad7e59090c3ddb49c5e39a77fb9d425d94b3
Alex Alan Nunes <alexalannunes@gmail.com>
2022-12-01 13:40:28 -0300

    test: add more test to control shadow

--- cypress/e2e/supa.cy.ts
+++ cypress/e2e/supa.cy.ts
@@ -10,4 +10,35 @@ describe("supa shadows", () => {
       "false"
     );
   });
+
+  it("toggle active shadow 1", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-testid="switch-active"]').click();
+    cy.get('[data-testid="switch-active"] > input').should("not.be.checked");
+  });
+
+  it("toggle inset shadow 1", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-testid="switch-inset"]').click();
+    cy.get('[data-testid="switch-inset"] > input').should("be.checked");
+  });
+
+  it("add new shadow", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('button[title="add new shadow"]').click();
+    cy.get('[data-testid="accordion-item-1"]').should(
+      "have.attr",
+      "aria-expanded",
+      "true"
+    );
+  });
+
+  it("remove shadow", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('button[title="add new shadow"]').click();
+
+    cy.wait(1000);
+    cy.get('[aria-label="Remove shadow"]').eq(1).click();
+    cy.get('[aria-label="Remove shadow"]').eq(1).should("not.exist");
+  });
 });
