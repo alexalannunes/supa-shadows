88bbe35254c221e796a109155d206c14eacc1425
Alex Alan Nunes <alexalannunes@gmail.com>
2022-12-01 21:00:46 -0300

    refactor: add custom input color on background box

--- components/shadow-properties/background-field.tsx
+++ components/shadow-properties/background-field.tsx
@@ -1,6 +1,7 @@
-import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { backgroundAtom } from "../../pages";
+import { CustomInputColor } from "../custom-input-color";
 
 export function BackgroundField() {
   const [background, setBackground] = useAtom(backgroundAtom);
@@ -8,13 +9,16 @@ export function BackgroundField() {
   return (
     <FormControl>
       <FormLabel fontWeight="bold">Background color</FormLabel>
-      <Input
-        type="text"
-        value={background}
-        onChange={(e) => setBackground(e.target.value)}
-        maxLength={7}
-        fontFamily="monospace"
-      />
+      <Flex gap={2} alignItems="center">
+        <CustomInputColor color={background} onChange={setBackground} />
+        <Input
+          type="text"
+          value={background}
+          onChange={(e) => setBackground(e.target.value)}
+          maxLength={7}
+          fontFamily="monospace"
+        />
+      </Flex>
     </FormControl>
   );
 }
