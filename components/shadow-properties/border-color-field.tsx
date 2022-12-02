894dd476efab7c90e55933e4518e4cb23fdbe227
Alex Alan Nunes <alexalannunes@gmail.com>
2022-12-02 13:06:20 -0300

    refactor: add custom input color on border color

--- components/shadow-properties/border-color-field.tsx
+++ components/shadow-properties/border-color-field.tsx
@@ -1,6 +1,7 @@
-import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { borderColorAtom } from "../../pages";
+import { CustomInputColor } from "../custom-input-color";
 
 export function BorderColorField() {
   const [borderColor, setBorderColor] = useAtom(borderColorAtom);
@@ -8,13 +9,17 @@ export function BorderColorField() {
   return (
     <FormControl>
       <FormLabel fontWeight="bold">Border color</FormLabel>
-      <Input
-        type="text"
-        value={borderColor}
-        onChange={(e) => setBorderColor(e.target.value)}
-        maxLength={7}
-        fontFamily="monospace"
-      />
+      <Flex alignItems="center" gap={2}>
+        <CustomInputColor color={borderColor} onChange={setBorderColor} />
+
+        <Input
+          type="text"
+          value={borderColor}
+          onChange={(e) => setBorderColor(e.target.value)}
+          maxLength={7}
+          fontFamily="monospace"
+        />
+      </Flex>
     </FormControl>
   );
 }
