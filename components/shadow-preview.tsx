875949a7aa8008c5f9a8d58c05bf7cb80c0ec735
Alex Alan Nunes <alexalannunes@gmail.com>
2022-12-03 11:10:52 -0300

    refactor: try implement mobile

--- components/shadow-preview.tsx
+++ components/shadow-preview.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { memo } from "react";
-import { useAtomValue } from "jotai";
+import { memo, useEffect } from "react";
+import { useAtom, useAtomValue } from "jotai";
 import {
   backgroundAtom,
   borderColorAtom,
@@ -10,21 +10,37 @@ import {
   shadowStringAtom,
   widthAtom,
 } from "../pages";
+import { useMd } from "../hooks/breakpoints";
 
 export function Component() {
   const color = useAtomValue(colorAtom);
   const background = useAtomValue(backgroundAtom);
-  const width = useAtomValue(widthAtom);
-  const height = useAtomValue(heightAtom);
+  const [width, setWidth] = useAtom(widthAtom);
+  const [height, setHeight] = useAtom(heightAtom);
   const borderColor = useAtomValue(borderColorAtom);
   const borderRadius = useAtomValue(borderRadiusAtom);
   const shadowString = useAtomValue(shadowStringAtom);
+
+  const [isMd] = useMd();
+
+  useEffect(() => {
+    if (!isMd) {
+      setWidth(10);
+      setHeight(10);
+      return;
+    }
+
+    setWidth(16);
+    setHeight(16);
+  }, [isMd, setWidth, setHeight]);
+
   return (
     <Flex
       as="main"
       flex={1}
       flexShrink={0}
-      alignItems="center"
+      alignItems={isMd ? "center" : "flex-start"}
+      paddingTop={isMd ? 0 : "3rem"}
       justifyContent="center"
       bg={color}
     >
