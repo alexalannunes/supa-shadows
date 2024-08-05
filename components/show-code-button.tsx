80f8343a151edc7244baa5bdc0bf76858f2af634
Alex Alan Nunes <alexalannunes@gmail.com>
2024-08-05 12:00:35 -0300

    hide button when has not utility

--- components/show-code-button.tsx
+++ components/show-code-button.tsx
@@ -1,8 +1,13 @@
 import { Button, useDisclosure } from "@chakra-ui/react";
 import { CopyCodeDialog } from "./copy-code-dialog";
+import { useRouter } from "next/router";
 
 export function CopyCodeButton() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const router = useRouter();
+
+  if (router.pathname === "/examples") return null;
 
   return (
     <>
