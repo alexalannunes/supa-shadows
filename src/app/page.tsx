9ff1d660c709eb8062d47bd8aee5f989b5facc77
Alex Alan Nunes <alexalannunes@gmail.com>
2025-08-19 20:05:42 -0300

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,20 @@ import { BoxPropsPanel } from "@/components/box-props-panel";
 import { BoxShadowPreviewPanel } from "@/components/box-shadow-preview-panel";
 import { Header } from "@/components/header";
 import { ShadowPropsPanel } from "@/components/shadow-props-panel";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
-      <main className="grid grid-cols-[25%_1fr_25%] flex-1">
-        <ShadowPropsPanel />
-        <BoxShadowPreviewPanel />
-        <BoxPropsPanel />
-      </main>
+      <Suspense>
+        <main className="grid grid-cols-[25%_1fr_25%] flex-1">
+          <ShadowPropsPanel />
+          <BoxShadowPreviewPanel />
+          <BoxPropsPanel />
+        </main>
+      </Suspense>
     </div>
   );
 }
